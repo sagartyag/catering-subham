8725911fd67d38deb6fdec5f64798fbf2e62bc93
Sachin <prajapatsachin835@gmail.com>
2024-07-03 17:53:04 +0530

    rr

--- resources/views/frontend/menu.blade.php
+++ resources/views/frontend/menu.blade.php
@@ -3,73 +3,74 @@
 <!-- Inner Pages Banner Section Start -->
 <div class="rac_inr_pages_banner rac_menu_banner">
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="rac_page_title">
-                    <h1>Menus</h1>
-                    <div class="rac_breadcrumbs">
-                        <a href="index.html">Home</a>
-                        <span>/</span>
-                        <p>Menus</p>
-                    </div>
-                </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="rac_page_title">
+            <h1>Menus</h1>
+            <div class="rac_breadcrumbs">
+              <a href="index.html">Home</a>
+              <span>/</span>
+              <p>Menus</p>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-<!-- Inner Pages Banner Section End -->
-
-<!-- Menu Section Start -->
-<div class="rac_section rac_menu_wrapper">
+  </div>
+  <!-- Inner Pages Banner Section End -->
+  <!-- Menu Section Start -->
+  <div class="rac_section rac_menu_wrapper">
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="rac_section_heading">
-                    <h5>Our Menu</h5>
-                    <h3>Most Popular Food In The World</h3>
-                </div>
-            </div>
-            <div class="col-12">               
-                <!-- Slick Slider for remaining categories -->
-                <ul class="rac_menu_tab slider">
+      <div class="row">
+        <div class="col-12">
+          <div class="rac_section_heading">
+            <h5>Our Menu</h5>
+            <h3>Most Popular Food In The World</h3>
+          </div>
+        </div>
+       
+      <div class="col-12">
+      <ul class="rac_menu_tab slider">
                     @foreach($categories as $index => $category)
-                        @if($index <= 12)
+                        @if($index >= 6)
                             <li>
                                 <a class="rac_btn category-tab" data-category-id="{{ $category->id }}" href="javascript:void(0)">{{ $category->categoryname }}</a>
                             </li>
                         @endif
                     @endforeach
                 </ul>
-            </div>
-
-            <div class="col-12">
-                <!-- Products list -->
-                <div class="rac_tab_pane" id="tab-1" style="display:block;">
-                    <div class="row" id="product-list">
-                        @foreach($products as $product)
-                            <div class="col-sm-6 product-item" data-category-id="{{ $product->category_id }}">
-                                <div class="rac_menu_box">
-                                    <div class="rac_menu_img">
-                                        <img src="{{ asset($product->image) }}" class="img-fluid" style="width:100px;">
-                                    </div>
-                                    <div class="rac_menu_text">
-                                        <div class="rac_menu_title">
-                                            <h4>{{ $product->productName }}</h4>
-                                            <!-- <h2>$ {{ $product->productPrice }}</h2> -->
-                                        </div>
-                                        <p>{{ $product->ProductDiscription }}</p>
-                                    </div>
-                                </div>
+      </div>
+      
+        <div class="col-12">
+          <!-- Starter Tab -->
+          <div class="rac_tab_pane" id="tab-1" style="display:block;">
+            <div class="row" id="product-list">
+                @foreach($products as $product)
+                    <div class="col-sm-6 product-item" data-category-id="{{ $product->category_id }}">
+                        <div class="rac_menu_box">
+                            <div class="rac_menu_img">
+                                <img src="{{ asset($product->image) }}" class="img-fluid" style="width:100px;;">
                             </div>
-                        @endforeach
+                            <div class="rac_menu_text">
+                                <div class="rac_menu_title">
+                                    <h4>{{ $product->productName }}</h4>
+                                    <!-- <h2>$ {{ $product->productPrice }}</h2> -->
+                                </div>
+                                <p>{{ $product->ProductDiscription }}</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
+         
+        </div>
+      </div>
     </div>
-</div>
-<!-- Menu Section End -->
-<div class="rac_section rac_offer_wrapper">
+  </div>
+  <!-- Menu Section End -->
+  <!-- Offer Zone Section Start -->
+  <div class="rac_section rac_offer_wrapper">
     <div class="container">
       <div class="row">
         <div class="col-12">
@@ -277,12 +278,39 @@
       </div>
     </div>
   </div>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function() {
+        var initialCategoryId = 1; // Set the initial category ID here
 
-@include('layouts.frontend.footer')
+        // Filter products based on initial category ID
+        $('.product-item').each(function() {
+            var productCategoryId = $(this).data('category-id');
+            if (productCategoryId != initialCategoryId) {
+                $(this).show();
+            }
+        });
 
+        $('.category-tab').on('click', function() {
+            var categoryId = $(this).data('category-id');
+
+            // Show all products initially
+            $('.product-item').hide();
+
+            // Filter products based on category
+            $('.product-item').each(function() {
+                var productCategoryId = $(this).data('category-id');
+                if (categoryId == 'all' || productCategoryId == categoryId) {
+                    $(this).show();
+                }
+            });
+        });
+    });
+</script>
 <!-- Include Slick Slider -->
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+
 
 <script>
     $(document).ready(function() {
@@ -291,7 +319,7 @@
             dots: false,
             infinite: true,
             speed: 300,
-            slidesToShow: 5,
+            slidesToShow: 3,
             slidesToScroll: 1,
             autoplay: true,
             autoplaySpeed: 2000,
@@ -299,7 +327,7 @@
                 {
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 3,
+                        slidesToShow: 2,
                         slidesToScroll: 1,
                         infinite: true,
                         dots: false
@@ -308,7 +336,7 @@
                 {
                     breakpoint: 600,
                     settings: {
-                        slidesToShow: 2,
+                        slidesToShow: 1,
                         slidesToScroll: 1
                     }
                 }
@@ -318,8 +346,14 @@
         // Filter products based on category tab click
         $('.category-tab').on('click', function() {
             var categoryId = $(this).data('category-id');
+
+            // Hide all products initially
             $('.product-item').hide();
+
+            // Show products based on category
             $('.product-item[data-category-id="' + categoryId + '"]').show();
         });
     });
 </script>
+
+  @include('layouts.frontend.footer')
