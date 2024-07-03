5292399aa3eafebf87421e68ab3060b527443d3e
Sachin <prajapatsachin835@gmail.com>
2024-07-03 17:23:05 +0530

    wd

--- resources/views/frontend/menu.blade.php
+++ resources/views/frontend/menu.blade.php
@@ -299,7 +299,7 @@
                 {
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 2,
+                        slidesToShow: 3,
                         slidesToScroll: 1,
                         infinite: true,
                         dots: false
@@ -308,7 +308,7 @@
                 {
                     breakpoint: 600,
                     settings: {
-                        slidesToShow: 1,
+                        slidesToShow: 2,
                         slidesToScroll: 1
                     }
                 }
